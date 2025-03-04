ae3f3a185b32a442862e0b81e198f8dfe65682dc
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-03-04 23:56:51 +0530

    signup and navbar is included

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes,Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
 function App() {
+  const Home = () => {
+    return <h2>Welcome to Home Page</h2>;
+  };
+  const Signin = () => {
+    return <h2>Welcome to signin Page</h2>;
+  };
+  const About= () => {
+    return <h2>Welcome to About Page</h2>;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+      <SignUp/>
+    </>
   );
 }
 
