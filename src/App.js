29be1a68662e885b168a29cfeed4082ee704ebb0
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-03-10 22:48:15 +0530

    login and signup routes are modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Routes,Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 function App() {
   const Home = () => {
     return <h2>Welcome to Home Page</h2>;
-  };
-  const Signin = () => {
-    return <h2>Welcome to signin Page</h2>;
   };
   const About= () => {
     return <h2>Welcome to About Page</h2>;
@@ -17,10 +15,10 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<Login/>}/>
         <Route path="/about" element={<About/>}/>
       </Routes>
-      <SignUp/>
     </>
   );
 }
