5df961e1db6c3c0aa93904983ccb7ad1d97b7e88
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-04-01 22:02:13 +0530

    logout is added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,28 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link,useNavigate} from "react-router-dom";
+import { getAuth,signOut } from "firebase/auth";
 import classes from "./Navbar.module.css";
 const Navbar=()=>{
+    const auth=getAuth();
+    const navigate=useNavigate();
+    const handleLogout=async()=>{
+        try{
+            await signOut(auth);
+            localStorage.removeItem('idToken');
+            navigate("/login");
+        }
+        catch(error){
+            console.log(error);
+            alert("logout failed");
+        }
+    };
     return(
         <nav className={classes.navbar}>
             <h1 className={classes["navbar-h1"]}>Expense Tracker Website</h1>
             <Link className={classes["navbar-link"]}to="/">Home</Link>
             <Link className={classes["navbar-link"]}to="/products">Products</Link>
-            <Link className={classes["navbar-link"]}to="/about">About Us</Link>   
+            <Link className={classes["navbar-link"]}to="/about">About Us</Link>
+            <button className={classes["navbar-button"]} onClick={handleLogout}>Log out</button>   
         </nav>
     );
 };
